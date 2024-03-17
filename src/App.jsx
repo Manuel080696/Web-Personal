cf018e9df71f8dc078c64f7329bdb4e90b9e2db6
Manuel Casa <mgmanugonza@gmail.com>
2024-03-17 09:13:35 +0100

    Primer commit, web personal

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/RRUvY572am8l9BCr/scene.splinecode" />
+  );
+}
